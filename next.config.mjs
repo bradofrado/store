54b0149724f4bbdc968ac136e6428301bbef03f5
Braydon Jones <bradofrado@gmail.com>
2024-09-04 13:19:31 -0600

    Add harmony stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        swcPlugins: [['harmony-ai-plugin', {rootDir: new URL(".", import.meta.url).pathname}]]
+    }
+};
 
 export default nextConfig;
