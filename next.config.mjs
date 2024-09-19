cc8eda944455a1ca3763ebbd24c909342cc9658c
Braydon Jones <bradofrado@gmail.com>
2024-09-19 14:08:02 -0600

    Add Harmony setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        swcPlugins: [['harmony-ai-plugin', {rootDir: new URL(".", import.meta.url).pathname}]]
+        swcPlugins: [['harmony-ai-plugin', {rootDir: new URL(".", import.meta.url).pathname, repositoryId: '7adebdf4-def2-479a-ab5e-c855bebe2d2c'}]]
     }
 };
 
