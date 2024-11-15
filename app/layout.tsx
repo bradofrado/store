4347d07e6bde64e5dd0252b34546ad6abda6dda7
Braydon Jones <bradofrado@gmail.com>
2024-11-14 23:04:27 -0700

    Add Harmony

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { QueryStateProvider } from '@/hooks/query-state';
+import { HarmonySetup } from 'harmony-ai-editor';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -20,7 +21,11 @@ export default function RootLayout({
     <ClerkProvider>
       <QueryStateProvider>
         <html lang='en'>
-          <body className={montserrat.className}>{children}</body>
+          <body className={montserrat.className}>
+            {children}
+
+            <HarmonySetup repositoryId='7adebdf4-def2-479a-ab5e-c855bebe2d2c' />
+          </body>
         </html>
       </QueryStateProvider>
     </ClerkProvider>
